efc89b18e4055fd06241c1c30e9b2f4fc92ae282
ximenanavarro <ximenanavarro@Ximenas-MacBook-Pro.local>
2023-01-12 11:47:43 -0700

    commit tree 0f91c6bdac6fbc7c143a752b2ce18c35d08d29b4

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,4 +1,5 @@
 class PlantsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   # GET /plants
   def index
@@ -8,7 +9,7 @@ class PlantsController < ApplicationController
 
   # GET /plants/:id
   def show
-    plant = Plant.find_by(id: params[:id])
+    plant = find_plant
     render json: plant
   end
 
@@ -18,9 +19,37 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  def update
+    plant = find_plant
+    if plant
+      plant.update(plant_params)
+      render json: plant
+    else
+      render_not_found_response
+    end
+  end
+
+  def destroy
+    plant = find_plant
+    if plant
+      plant.destroy
+      head :no_content
+    else
+      render_not_found_response
+    end
+  end
+
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
+  end
+
+  def find_plant
+    Plant.find(params[:id])
+  end
+
+  def render_not_found_response
+    render json: {error: "Plant not found"}, status: :not_found
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  resources :plants, only: [:index, :show, :create]
+  resources :plants
+  
 end
